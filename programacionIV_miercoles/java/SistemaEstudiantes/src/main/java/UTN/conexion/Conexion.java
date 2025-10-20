fb44f055a339f07e8cffc6c109f76aded0aa081f
mirenecarmen <32132733@alumnos.frsr.utn.edu.ar>
2025-10-20 00:12:07 -0300

    Agrego proyecto IntelliJ SistemaEstudiantes completo

---
+++ programacionIV_miercoles/java/SistemaEstudiantes/src/main/java/UTN/conexion/Conexion.java
@@ -0,0 +1,26 @@
+package UTN.conexion;
+
+import java.sql.Connection; //2ª aparece cuando hacemos "primero"
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+    public static Connection getConnection(){ //primero
+        Connection conexion = null;
+        //ahora crearemos variables para conectarnos a la base de datos
+        var baseDatos = "estudiantes2025";
+        var url = "jdbc:mysql://localhost:3306/" + baseDatos;
+        var usuario = "root";
+        var password = "admin";
+
+        // ahora cargamos la clase del driver de mysql en memoria
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, password);
+        } catch (ClassNotFoundException | SQLException e){
+            System.out.println("Ocurrio un error en la conexion:"+ e.getMessage());
+        }
+        //fin catch
+        return conexion;
+    } // fin metodo connection
+}
